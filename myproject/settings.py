807d3fd16d5eaedd3b1181dce6f3b6b46e7db1d7
Rohith Surya Podugu <prsurya1020@gmail.com>
2018-08-19 00:31:05 +0530

    STATIC_ROOT path new

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config, Csv
+import dj_database_url
+
+# DATABASE URL not changed
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
